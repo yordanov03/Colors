6a8a449e9e273257a408108b6f2623e53b7c7d0a
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2021-12-26 15:52:50 +0200

    some more

--- Application/Features/People/Queries/Common/PersonOutputModel.cs
+++ Application/Features/People/Queries/Common/PersonOutputModel.cs
@@ -1,13 +1,11 @@
-﻿using Domain.Models;
-
-namespace Application.Features.People.Queries.Common
+﻿namespace Application.Features.People.Queries.Common
 {
     public class PersonOutputModel
     {
-        public int Id { get;  set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get;  set; }
-        public string Zipcode { get;  set; }
+        public string LastName { get; set; }
+        public string Zipcode { get; set; }
         public string City { get; set; }
         public string Color { get; set; }
     }
